b7f809bd66aae86ce7cf6e6f2fcaca3e5e9ed0af
Dan Ball <danpelota@gmail.com>
2016-12-09 07:57:35 -0500

    Changed path of firefox binary - no need to specify path

--- functional_tests.py
+++ functional_tests.py
@@ -1,8 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 
-browser = webdriver.Firefox(firefox_binary=FirefoxBinary(
-    firefox_path='/home/dan/.local/bin/firefox-bin'))
+browser = webdriver.Firefox()
 browser.get('http://localhost:8000')
 
 assert 'Django' in browser.title
